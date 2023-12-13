8e3028a8a3dea71a72260fdb36340b4a0c4f8782
Vikram Naik <26vikramnaik@gmail.com>
2023-12-13 23:18:47 +0530

    initial commit and quick start

---
+++ quickStart/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = 'Vikram';
+console.log(userName);
+let a = 12;
+let b = '6';
+let c = 2;
+// console.log(a / b) // error
---
+++ quickStart/src/main.ts
@@ -0,0 +1,8 @@
+let userName = 'Vikram'
+console.log(userName)
+
+let a: number = 12
+let b: string = '6'
+let c: number = 2
+
+// console.log(a / b) // error
