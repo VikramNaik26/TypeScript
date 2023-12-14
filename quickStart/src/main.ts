11499936fd219943d5b0930f12b8ad1e30015654
Vikram Naik <26vikramnaik@gmail.com>
2023-12-14 22:06:54 +0530

    basic datatypes

--- quickStart/src/main.ts
+++ quickStart/src/main.ts
@@ -1,4 +1,4 @@
-let userName = 'Vikram'
+/* let userName = 'Vikram'
 console.log(userName)
 
 let a: number = 12
@@ -6,3 +6,17 @@ let b: string = '6'
 let c: number = 2
 
 // console.log(a / b) // error
+ */
+
+let myName: string
+myName = 'Vikram'
+let meaningOfLife: number = 44
+let isLoading: boolean = true
+let album: any
+
+const sum = (a: number, b: number) => a + b
+
+let postId: string | number
+let isActive: boolean | number
+
+let regex: RegExp = /\w+/g
